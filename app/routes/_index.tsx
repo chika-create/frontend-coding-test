17f246a9e55d2110f37300d659c1e40d6d717db8
chika <hawmea@yahoo.co.jp>
2024-05-27 22:17:42 +0900

    💄 都道府県のチェックボックスコンポーネントの枠を作成

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Prefectures } from "../../components/Prefectures"
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +12,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>都道府県別の総人口推移グラフ</h1>
+      <Prefectures />
     </div>
   );
 }
---
+++ components/Prefectures/Prefectures.tsx
@@ -0,0 +1,14 @@
+export const Prefectures = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                <label>
+                    <input type="checkbox" />
+                    都道府県名
+                </label>
+            </li>
+        </ul>
+    </div>
+  );
+};
