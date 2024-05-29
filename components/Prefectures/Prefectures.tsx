3160c175473992d02d77cb2d9a84f1ae3b78182a
安井 智香 <yasui.chika@a-tm.co.jp>
2024-05-29 20:37:53 +0900

    :recycle: 外部化したfetchヘルパー関数を読み込みに置き換え

--- components/Prefectures/Prefectures.tsx
+++ components/Prefectures/Prefectures.tsx
@@ -1,23 +1,5 @@
 import { FC, useEffect, useState } from "react";
-
-const fetchprefs = async (prefCode: number, API_KEY: string) => {
-  try {
-    const response = await fetch(
-      `https://opendata.resas-portal.go.jp/api/v1/prefectures`,
-      {
-        headers: { "X-API-KEY": API_KEY || "" },
-      }
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data.result;
-  } catch (error) {
-    console.error("Error fetching prefs:", error);
-    return [];
-  }
-};
+import { fetchprefs } from "../../lib/fetchReasasToken";
 
 export const Prefectures: FC<{ prefCode: number; apikey: string }> = ({
   prefCode,
