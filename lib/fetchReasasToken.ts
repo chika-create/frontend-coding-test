e0f22ba345d9bad6ebf2c609e7278d7219d2fa77
安井 智香 <yasui.chika@a-tm.co.jp>
2024-05-29 20:36:10 +0900

    :recycle: ResasAPIのfetch処理を外部化

---
+++ lib/fetchReasasToken.ts
@@ -0,0 +1,18 @@
+export const fetchprefs = async (prefCode: number, API_KEY: string) => {
+  try {
+    const response = await fetch(
+      `https://opendata.resas-portal.go.jp/api/v1/prefectures`,
+      {
+        headers: { "X-API-KEY": API_KEY || "" },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    console.error("Error fetching prefs:", error);
+    return [];
+  }
+};
