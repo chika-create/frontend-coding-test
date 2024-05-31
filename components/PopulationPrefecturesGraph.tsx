920c9b0ac0c36c40fbb3b84cc2b0d2c4e5a1afdc
安井 智香 <yasui.chika@a-tm.co.jp>
2024-05-31 19:21:45 +0900

    :+1: Prefectures_都道府県の選択管理するコードを上位のコンポーネントに移動

--- components/PopulationPrefecturesGraph.tsx
+++ components/PopulationPrefecturesGraph.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Prefectures } from "./Prefectures";
 import { PopulationGraph } from "./PopulationGraph";
 
@@ -7,9 +7,31 @@ export const PopulationPrefecturesGraph: FC<{
 }> = ({ apikey }) => {
   const prefCode = 0;
 
+  // チェックボックスの変更を処理する関数
+  const handleCheckboxChange = (code: string) => {
+    console.log("code: ", code);
+    setSelectedPrefs((currentSelected) => {
+      if (currentSelected.includes(code)) {
+        // 既に選択されている場合は除去
+        return currentSelected.filter((selectedCode) => selectedCode !== code);
+      } else {
+        // 選択されていない場合は追加
+        return [...currentSelected, code];
+      }
+    });
+  };
+
+  // 選択された都道府県コードを管理するための状態
+  const [selectedPrefs, setSelectedPrefs] = useState<string[]>([]);
+
   return (
     <div>
-      <Prefectures prefCode={prefCode} apikey={apikey} />
+      <Prefectures
+        prefCode={prefCode}
+        apikey={apikey}
+        handleCheckboxChange={handleCheckboxChange}
+        selectedPrefs={selectedPrefs}
+      />
       <PopulationGraph />
     </div>
   );
--- components/Prefectures/Prefectures.tsx
+++ components/Prefectures/Prefectures.tsx
@@ -5,7 +5,9 @@ import { PrefecturesItem } from "./PrefecturesItem";
 export const Prefectures: FC<{
   prefCode: number;
   apikey: string;
-}> = ({ prefCode, apikey }) => {
+  handleCheckboxChange: any;
+  selectedPrefs: any;
+}> = ({ prefCode, apikey, handleCheckboxChange, selectedPrefs }) => {
   const [prefs, setprefs] = useState<{ prefCode: string; prefName: string }[]>(
     []
   );
@@ -25,24 +27,6 @@ export const Prefectures: FC<{
       setLoading(false);
     })();
   }, [prefCode, apikey]);
-
-  // 選択された都道府県コードを管理するための状態
-  const [selectedPrefs, setSelectedPrefs] = useState<string[]>([]);
-
-  // チェックボックスの変更を処理する関数
-  const handleCheckboxChange = (code: string) => {
-    console.log("code: ", code);
-    console.log("hoge");
-    setSelectedPrefs((currentSelected) => {
-      if (currentSelected.includes(code)) {
-        // 既に選択されている場合は除去
-        return currentSelected.filter((selectedCode) => selectedCode !== code);
-      } else {
-        // 選択されていない場合は追加
-        return [...currentSelected, code];
-      }
-    });
-  };
 
   return (
     <div>
