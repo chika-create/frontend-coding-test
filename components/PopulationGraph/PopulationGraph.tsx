8c3c529c02cba2c73c69b1897e8c29885bf38e0f
chika <hawmea@yahoo.co.jp>
2024-06-01 12:12:27 +0900

    ✨ sparkles: 二重線の仮グラフを作成

--- components/PopulationGraph/PopulationGraph.tsx
+++ components/PopulationGraph/PopulationGraph.tsx
@@ -1,31 +1,51 @@
-import { FC } from "react";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-const data = [{name: 'A', uv: 400, pv: 2400, amt: 2400}, {name: 'B', uv: 800, pv: 3000, amt: 2600}, {name: 'C', uv: 300, pv: 2000, amt: 2000}];
+import React, { FC } from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+];
 
 export const PopulationGraph: FC = () => {
-  // const [prefs, setprefs] = useState<{ prefCode: string; prefName: string }[]>(
-  //   []
-  // );
-  // const [selectedprefs, setSelectedprefs] = useState<string[]>([]);
+  // const [prefs, setPrefs] = useState<{ prefCode: string; prefName: string }[]>([]);
+  // const [selectedPrefs, setSelectedPrefs] = useState<string[]>([]);
 
   return (
     <>
       <div>
         <h2>選択された都道府県:</h2>
         <ul>
-          {/* {selectedprefs.map((prefCode) => {
+          {/* {selectedPrefs.map((prefCode) => {
             const pref = prefs.find((c) => c.prefCode === prefCode);
             return <li key={prefCode}>{pref?.prefName}</li>;
           })} */}
         </ul>
       </div>
-      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-      </LineChart>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis yAxisId="left" />
+          <YAxis yAxisId="right" orientation="right" />
+          <Tooltip />
+          <Legend />
+          <Line yAxisId="left" type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     </>
   );
 };
