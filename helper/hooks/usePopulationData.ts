8f54f01249e0c303f9563b1a84268c55f9b222a7
安井 智香 <yasui.chika@a-tm.co.jp>
2024-07-03 09:36:11 +0900

    :recycle: 各都道府県の人口データを格納取得をカスタムHookに切り出し

---
+++ helper/hooks/usePopulationData.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { useFetchApiKey } from "../../helper/hooks/useFetchApiKey";
+import { fetchPopulationData } from "../../lib/fetchPopulationData";
+
+interface PopulationData {
+  year: number;
+  value: number;
+}
+
+export const usePopulationData = ({ selectedPrefs }: any) => {
+  const apikey = useFetchApiKey();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [populationData, setPopulationData] = useState<{
+    [key: string]: PopulationData[];
+  }>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      const newPopulationData: { [key: string]: PopulationData[] } = {};
+      for (const prefCode of selectedPrefs) {
+        try {
+          const data = await fetchPopulationData(apikey, Number(prefCode));
+          if (data.length === 0) {
+            setError("Failed to fetch population data");
+          } else {
+            newPopulationData[prefCode] = data;
+          }
+        } catch (e) {
+          setError(`Failed to fetch population data for prefCode: ${prefCode}`);
+        }
+      }
+      setPopulationData(newPopulationData);
+      setLoading(false);
+    };
+    fetchData();
+  }, [selectedPrefs, apikey]);
+};
